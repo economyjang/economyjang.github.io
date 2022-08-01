3737be7e0eeacf876ffcf174baee9829e9c12a3e
KyungJae-Jang <kyungjaejang@gmail.com>
2022-08-01 21:38:09 +0900

    Updates

--- app/immutable/pages/post/javascript/2.md-cd5db224.js
+++ app/immutable/pages/post/javascript/2.md-cd5db224.js
@@ -1,4 +1,4 @@
-import{S as V,i as W,s as X,D as q,x as Y,y as Z,z as ee,A as ae,B as K,r as oe,p as te,C as ne,R as L,e as m,t as u,k as j,c as r,a as f,h as y,d as t,m as I,b as C,g as s,F as d,n as de}from"../../../chunks/index-08721170.js";import{P as le}from"../../../chunks/_post-4c8e9188.js";import"../../../chunks/stores-c56a77d9.js";function ie(c){let a,h,w,l,o,i,H,Q,p,$,U,v,E,S,P,x,g,R,N,k,_,b,G;return{c(){a=m("h1"),h=u("Hello my first page"),w=j(),l=m("p"),o=m("strong"),i=u("Nice to meet you!"),H=u(`
+import{S as K,i as W,s as X,D as q,x as Y,y as Z,z as ee,A as ae,B as L,r as oe,p as te,C as ne,R as M,e as m,t as u,k as j,c as r,a as f,h as y,d as t,m as I,b as C,g as s,F as d,n as de}from"../../../chunks/index-08721170.js";import{P as le}from"../../../chunks/_post-f3af2d7f.js";import"../../../chunks/stores-c56a77d9.js";function ie(c){let a,h,w,l,o,i,H,Q,p,$,U,g,E,b,S,x,v,T,N,k,_,P,R;return{c(){a=m("h1"),h=u("Hello my first page"),w=j(),l=m("p"),o=m("strong"),i=u("Nice to meet you!"),H=u(`
 Updating Quina to version 2.0 filled my head with new ideas that I just couldn\u2019t walk away from. Eventually,
 those ideas took the shape of a whole new word game, named Hondo.
 Updating Quina to version 2.0 filled my head with new ideas that I just couldn\u2019t walk away from. Eventually,
@@ -13,7 +13,7 @@ Updating Quina to version 2.0 filled my head with new ideas that I just couldn\u
 those ideas took the shape of a whole new word game, named Hondo.Updating Quina to version 2.0 filled my head with new ideas that I just couldn\u2019t walk away from. Eventually,
 those ideas took the shape of a whole new word game, named Hondo.
 Updating Quina to version 2.0 filled my head with new ideas that I just couldn\u2019t walk away from. Eventually,
-those ideas took the shape of a whole new word game, named Hondo.`),Q=j(),p=m("h1"),$=u("Hello my second page"),U=j(),v=m("p"),E=m("strong"),S=u("Nice to meet you!"),P=u(`
+those ideas took the shape of a whole new word game, named Hondo.`),Q=j(),p=m("h1"),$=u("Hello my second page"),U=j(),g=m("p"),E=m("strong"),b=u("Nice to meet you!"),S=u(`
 Updating Quina to version 2.0 filled my head with new ideas that I just couldn\u2019t walk away from. Eventually,
 those ideas took the shape of a whole new word game, named Hondo.
 Updating Quina to version 2.0 filled my head with new ideas that I just couldn\u2019t walk away from. Eventually,
@@ -28,7 +28,7 @@ Updating Quina to version 2.0 filled my head with new ideas that I just couldn\u
 those ideas took the shape of a whole new word game, named Hondo.Updating Quina to version 2.0 filled my head with new ideas that I just couldn\u2019t walk away from. Eventually,
 those ideas took the shape of a whole new word game, named Hondo.
 Updating Quina to version 2.0 filled my head with new ideas that I just couldn\u2019t walk away from. Eventually,
-those ideas took the shape of a whole new word game, named Hondo.`),x=j(),g=m("h1"),R=u("Hello my third page"),N=j(),k=m("p"),_=m("strong"),b=u("Nice to meet you!"),G=u(`
+those ideas took the shape of a whole new word game, named Hondo.`),x=j(),v=m("h1"),T=u("Hello my third page"),N=j(),k=m("p"),_=m("strong"),P=u("Nice to meet you!"),R=u(`
 Updating Quina to version 2.0 filled my head with new ideas that I just couldn\u2019t walk away from. Eventually,
 those ideas took the shape of a whole new word game, named Hondo.
 Updating Quina to version 2.0 filled my head with new ideas that I just couldn\u2019t walk away from. Eventually,
@@ -43,7 +43,7 @@ Updating Quina to version 2.0 filled my head with new ideas that I just couldn\u
 those ideas took the shape of a whole new word game, named Hondo.Updating Quina to version 2.0 filled my head with new ideas that I just couldn\u2019t walk away from. Eventually,
 those ideas took the shape of a whole new word game, named Hondo.
 Updating Quina to version 2.0 filled my head with new ideas that I just couldn\u2019t walk away from. Eventually,
-those ideas took the shape of a whole new word game, named Hondo.`),this.h()},l(e){a=r(e,"H1",{id:!0});var n=f(a);h=y(n,"Hello my first page"),n.forEach(t),w=I(e),l=r(e,"P",{});var O=f(l);o=r(O,"STRONG",{});var z=f(o);i=y(z,"Nice to meet you!"),z.forEach(t),H=y(O,`
+those ideas took the shape of a whole new word game, named Hondo.`),this.h()},l(e){a=r(e,"H1",{id:!0});var n=f(a);h=y(n,"Hello my first page"),n.forEach(t),w=I(e),l=r(e,"P",{});var G=f(l);o=r(G,"STRONG",{});var z=f(o);i=y(z,"Nice to meet you!"),z.forEach(t),H=y(G,`
 Updating Quina to version 2.0 filled my head with new ideas that I just couldn\u2019t walk away from. Eventually,
 those ideas took the shape of a whole new word game, named Hondo.
 Updating Quina to version 2.0 filled my head with new ideas that I just couldn\u2019t walk away from. Eventually,
@@ -58,7 +58,7 @@ Updating Quina to version 2.0 filled my head with new ideas that I just couldn\u
 those ideas took the shape of a whole new word game, named Hondo.Updating Quina to version 2.0 filled my head with new ideas that I just couldn\u2019t walk away from. Eventually,
 those ideas took the shape of a whole new word game, named Hondo.
 Updating Quina to version 2.0 filled my head with new ideas that I just couldn\u2019t walk away from. Eventually,
-those ideas took the shape of a whole new word game, named Hondo.`),O.forEach(t),Q=I(e),p=r(e,"H1",{id:!0});var B=f(p);$=y(B,"Hello my second page"),B.forEach(t),U=I(e),v=r(e,"P",{});var T=f(v);E=r(T,"STRONG",{});var D=f(E);S=y(D,"Nice to meet you!"),D.forEach(t),P=y(T,`
+those ideas took the shape of a whole new word game, named Hondo.`),G.forEach(t),Q=I(e),p=r(e,"H1",{id:!0});var A=f(p);$=y(A,"Hello my second page"),A.forEach(t),U=I(e),g=r(e,"P",{});var O=f(g);E=r(O,"STRONG",{});var D=f(E);b=y(D,"Nice to meet you!"),D.forEach(t),S=y(O,`
 Updating Quina to version 2.0 filled my head with new ideas that I just couldn\u2019t walk away from. Eventually,
 those ideas took the shape of a whole new word game, named Hondo.
 Updating Quina to version 2.0 filled my head with new ideas that I just couldn\u2019t walk away from. Eventually,
@@ -73,7 +73,7 @@ Updating Quina to version 2.0 filled my head with new ideas that I just couldn\u
 those ideas took the shape of a whole new word game, named Hondo.Updating Quina to version 2.0 filled my head with new ideas that I just couldn\u2019t walk away from. Eventually,
 those ideas took the shape of a whole new word game, named Hondo.
 Updating Quina to version 2.0 filled my head with new ideas that I just couldn\u2019t walk away from. Eventually,
-those ideas took the shape of a whole new word game, named Hondo.`),T.forEach(t),x=I(e),g=r(e,"H1",{id:!0});var F=f(g);R=y(F,"Hello my third page"),F.forEach(t),N=I(e),k=r(e,"P",{});var A=f(k);_=r(A,"STRONG",{});var J=f(_);b=y(J,"Nice to meet you!"),J.forEach(t),G=y(A,`
+those ideas took the shape of a whole new word game, named Hondo.`),O.forEach(t),x=I(e),v=r(e,"H1",{id:!0});var F=f(v);T=y(F,"Hello my third page"),F.forEach(t),N=I(e),k=r(e,"P",{});var B=f(k);_=r(B,"STRONG",{});var J=f(_);P=y(J,"Nice to meet you!"),J.forEach(t),R=y(B,`
 Updating Quina to version 2.0 filled my head with new ideas that I just couldn\u2019t walk away from. Eventually,
 those ideas took the shape of a whole new word game, named Hondo.
 Updating Quina to version 2.0 filled my head with new ideas that I just couldn\u2019t walk away from. Eventually,
@@ -88,4 +88,4 @@ Updating Quina to version 2.0 filled my head with new ideas that I just couldn\u
 those ideas took the shape of a whole new word game, named Hondo.Updating Quina to version 2.0 filled my head with new ideas that I just couldn\u2019t walk away from. Eventually,
 those ideas took the shape of a whole new word game, named Hondo.
 Updating Quina to version 2.0 filled my head with new ideas that I just couldn\u2019t walk away from. Eventually,
-those ideas took the shape of a whole new word game, named Hondo.`),A.forEach(t),this.h()},h(){C(a,"id","hello_my_first_page"),C(p,"id","hello_my_second_page"),C(g,"id","hello_my_third_page")},m(e,n){s(e,a,n),d(a,h),s(e,w,n),s(e,l,n),d(l,o),d(o,i),d(l,H),s(e,Q,n),s(e,p,n),d(p,$),s(e,U,n),s(e,v,n),d(v,E),d(E,S),d(v,P),s(e,x,n),s(e,g,n),d(g,R),s(e,N,n),s(e,k,n),d(k,_),d(_,b),d(k,G)},p:de,d(e){e&&t(a),e&&t(w),e&&t(l),e&&t(Q),e&&t(p),e&&t(U),e&&t(v),e&&t(x),e&&t(g),e&&t(N),e&&t(k)}}}function se(c){let a,h;const w=[c[0],M];let l={$$slots:{default:[ie]},$$scope:{ctx:c}};for(let o=0;o<w.length;o+=1)l=q(l,w[o]);return a=new le({props:l}),{c(){Y(a.$$.fragment)},l(o){Z(a.$$.fragment,o)},m(o,i){ee(a,o,i),h=!0},p(o,[i]){const H=i&1?ae(w,[i&1&&K(o[0]),i&0&&K(M)]):{};i&2&&(H.$$scope={dirty:i,ctx:o}),a.$set(H)},i(o){h||(oe(a.$$.fragment,o),h=!0)},o(o){te(a.$$.fragment,o),h=!1},d(o){ne(a,o)}}}const M={title:"Announcing Hondo",published:"2022.07.20",updated:"2022.07.20",summary:"Updating Quina to version 2.0 filled my head with new ideas that I just couldn't walk away from. Eventually, those ideas took the shape of a whole new word game, named Hondo.",tags:["Javascript","Svelte"],index:["#hello_my_first_page","#hello_my_second_page","#hello_my_third_page"]};function he(c,a,h){return c.$$set=w=>{h(0,a=q(q({},a),L(w)))},a=L(a),[a]}class re extends V{constructor(a){super(),W(this,a,he,se,X,{})}}export{re as default,M as metadata};
+those ideas took the shape of a whole new word game, named Hondo.`),B.forEach(t),this.h()},h(){C(a,"id","hello_my_first_page"),C(p,"id","hello_my_second_page"),C(v,"id","hello_my_third_page")},m(e,n){s(e,a,n),d(a,h),s(e,w,n),s(e,l,n),d(l,o),d(o,i),d(l,H),s(e,Q,n),s(e,p,n),d(p,$),s(e,U,n),s(e,g,n),d(g,E),d(E,b),d(g,S),s(e,x,n),s(e,v,n),d(v,T),s(e,N,n),s(e,k,n),d(k,_),d(_,P),d(k,R)},p:de,d(e){e&&t(a),e&&t(w),e&&t(l),e&&t(Q),e&&t(p),e&&t(U),e&&t(g),e&&t(x),e&&t(v),e&&t(N),e&&t(k)}}}function se(c){let a,h;const w=[c[0],V];let l={$$slots:{default:[ie]},$$scope:{ctx:c}};for(let o=0;o<w.length;o+=1)l=q(l,w[o]);return a=new le({props:l}),{c(){Y(a.$$.fragment)},l(o){Z(a.$$.fragment,o)},m(o,i){ee(a,o,i),h=!0},p(o,[i]){const H=i&1?ae(w,[i&1&&L(o[0]),i&0&&L(V)]):{};i&2&&(H.$$scope={dirty:i,ctx:o}),a.$set(H)},i(o){h||(oe(a.$$.fragment,o),h=!0)},o(o){te(a.$$.fragment,o),h=!1},d(o){ne(a,o)}}}const V={title:"Building accessible toggle buttons (with examples for Svelte, Vue, and React)",published:"2022.07.25",updated:"2022.07.25",summary:"Toggle buttons might be the biggest missing native HTML element. This post will help you code them right, for all users, in any (or no) framework.",tags:["Javascript","Svelte"],index:["#hello_my_first_page","#hello_my_second_page","#hello_my_third_page"]};function he(c,a,h){return c.$$set=w=>{h(0,a=q(q({},a),M(w)))},a=M(a),[a]}class re extends K{constructor(a){super(),W(this,a,he,se,X,{})}}export{re as default,V as metadata};
